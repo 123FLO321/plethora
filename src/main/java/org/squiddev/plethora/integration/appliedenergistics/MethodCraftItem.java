00b279351513e95cc26ee11b71eaf82ba96f55b3
Florian Kostenzer <florian.kostenzer@icloud.com>
2023-03-06 19:37:07 +0100

    fix: use fake player for requesting items

--- src/main/java/org/squiddev/plethora/integration/appliedenergistics/MethodCraftItem.java
+++ src/main/java/org/squiddev/plethora/integration/appliedenergistics/MethodCraftItem.java
@@ -4,15 +4,20 @@ import appeng.api.networking.IGrid;
 import appeng.api.networking.IGridNode;
 import appeng.api.networking.crafting.ICraftingGrid;
 import appeng.api.networking.security.IActionHost;
+import appeng.api.networking.security.IActionSource;
 import appeng.api.storage.data.IAEItemStack;
 import appeng.core.AppEng;
-import appeng.me.helpers.MachineSource;
+import appeng.me.helpers.PlayerSource;
 import dan200.computercraft.api.peripheral.IComputerAccess;
+import net.minecraft.world.WorldServer;
+import net.minecraftforge.common.util.FakePlayer;
 import org.squiddev.plethora.api.method.ContextKeys;
 import org.squiddev.plethora.api.method.IContext;
 import org.squiddev.plethora.api.method.TypedLuaObject;
 import org.squiddev.plethora.api.method.wrapper.FromContext;
+import org.squiddev.plethora.api.method.wrapper.Optional;
 import org.squiddev.plethora.api.method.wrapper.PlethoraMethod;
+import org.squiddev.plethora.gameplay.PlethoraFakePlayer;
 
 import javax.annotation.Nonnull;
 
@@ -36,8 +41,9 @@ public final class MethodCraftItem {
 		toCraft.setStackSize(quantity);
 
 		IComputerAccess computer = context.getContext(ContextKeys.COMPUTER, IComputerAccess.class);
-		CraftingResult result = new CraftingResult(grid, computer, host);
-		crafting.beginCraftingJob(node.getWorld(), grid, new MachineSource(host), toCraft, result.getCallback());
+		CraftingResult result = new CraftingResult(grid, computer, host, virtual);
+		IActionSource source = new PlayerSource(new FakePlayer((WorldServer) host.getActionableNode().getWorld(), PlethoraFakePlayer.PROFILE), host);
+		crafting.beginCraftingJob(node.getWorld(), grid, source, toCraft, result.getCallback());
 
 		return context.makeChildId(result).getObject();
 	}
